cc295ff41473fbb2fa09a59751484b96524af22d
Liren21 <kuminovvv21@gmail.com>
2022-11-01 23:08:13 +0600

    change bottomPanel

--- src/app/components/pages/BottomPanel/BottomPanel.tsx
+++ src/app/components/pages/BottomPanel/BottomPanel.tsx
@@ -3,35 +3,39 @@ import { observer } from 'mobx-react-lite'
 import './BottomPanel.scss'
 import { Button, Col, Row } from 'react-bootstrap'
 import routes from '../../../lib/routes'
+import { nanoid } from 'nanoid'
 
 export default observer(() => {
+    const btnForm = [
+        {
+            title: 'Займ',
+            icon: 'fa fa-credit-card',
+            href: '#' + routes.HOME,
+        },
+        {
+            title: 'Калькулято',
+            icon: 'fa fa-bar-chart',
+            href: '#' + routes.LOAN_PORTFOLIO,
+        },
+        {
+            title: 'Избранное',
+            icon: 'fa fa-heart-o',
+            href: '#',
+        },
+    ]
     return (
         <div className={'bottom-panel'}>
             <Row style={{ display: 'inline-block' }}>
-                <Col md={4} sm={4} xs={4} style={{ display: 'inline-block' }}>
-                    <Button href={'#' + routes.HOME} className={'btn-bottom-panel'} variant="light">
-                        <div>
-                            <i className="fa fa-credit-card" aria-hidden="true" />
-                        </div>
-                        Займы
-                    </Button>{' '}
-                </Col>
-                <Col md={4} sm={4} xs={4} style={{ display: 'inline-block' }}>
-                    <Button href={'#' + routes.LOAN_PORTFOLIO} className={'btn-bottom-panel'} variant="light">
-                        <div>
-                            <i className="fa fa-bar-chart" aria-hidden="true" />
-                        </div>
-                        Калькулято
-                    </Button>{' '}
-                </Col>
-                <Col md={4} sm={4} xs={4} style={{ display: 'inline-block' }}>
-                    <Button className={'btn-bottom-panel'} variant="light">
-                        <div>
-                            <i className="fa fa-heart-o" aria-hidden="true"></i>
-                        </div>
-                        Избранное
-                    </Button>{' '}
-                </Col>
+                {btnForm.map((data) => (
+                    <Col key={nanoid()} md={4} sm={4} xs={4} style={{ display: 'inline-block' }}>
+                        <Button href={data.href} className={'btn-bottom-panel'} variant="light">
+                            <div>
+                                <i className={data.icon} aria-hidden="true" />
+                            </div>
+                            {data.title}
+                        </Button>
+                    </Col>
+                ))}
             </Row>
         </div>
     )
